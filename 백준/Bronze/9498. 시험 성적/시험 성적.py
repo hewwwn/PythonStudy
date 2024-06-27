0e78adec434c1d8047435e033d980be739bdd359
Jang Hee Won <jhw001013@kyonggi.ac.kr>
2024-06-28 00:34:39 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+x = int(input())
+if 90 <= x and 100 >= x:
+    print('A')
+elif 80 <= x and 89 >= x:
+    print('B')
+elif 70 <= x and 79 >= x:
+    print('C')
+elif 60 <= x and 69 >= x:
+    print('D')
+else:
+    print('F')
